9533a44a95e5ee8e292624f7e8bbb8fb28802a34
JackReis01 <jackreis2019@gmail.com>
2017-09-01 08:20:06 -0500

    Assessment

---
+++ Standard/src/standards/Test3.java
@@ -0,0 +1,14 @@
+package standards;
+import javax.swing.JOptionPane;
+public class Test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Why must I do all this?");
+		
+		/*System.out.println("Brosius is not cool for the no backpack rule.");
+		 *  This one is commented out
+		 */
+	}
+
+}
